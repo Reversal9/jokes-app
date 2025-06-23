903ec0c6834a2b13c8fd8f5469c23c629966ecae
Jonnen Chong <jonnenc@gmail.com>
2025-06-23 10:51:04 -0500

    generate prisma client

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello!')
+  res.send('Hello!')
+})
+
+const url = 'https://sv443.net/jokeapi/v2/joke/'
+
+app.get('/joke', await(req, res) => {
+  const response = await fetch(url)
+
+  const data = await response.json()
+
+  res.send({
+    statusCode: 200,
+    body: data.data
+  })
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
