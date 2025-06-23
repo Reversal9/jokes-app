17da9de4db00474371dc1d046af32889361793f8
Jonnen Chong <jonnenc@gmail.com>
2025-06-23 11:15:00 -0500

    add joke and log buttons to streamlit

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,3 +1,30 @@
 import streamlit as st
+import requests
+from datetime import datetime
 
 st.write("Hello World!")
+
+if st.button("Get Joke", type="primary", key="joke"):
+    start = datetime.now()
+
+    data = requests.get("http://backend:3000/joke").json()
+
+    if "joke" in data.get("body"):
+        st.write(data.get("body").get("joke"))
+    else:
+        st.write(data.get("body").get("setup"))
+        st.write(data.get("body").get("delivery"))
+
+    end = datetime.now()
+    st.write(end-start)
+
+st.write("History")
+
+if st.button("Get Log", type="primary", key="log"):
+    start = datetime.now()
+
+    data = requests.get("http://backend:3000/log").json()
+    st.write(data)
+
+    end = datetime.now()
+    st.write(end-start)
